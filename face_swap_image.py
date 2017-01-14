8d574ea12531a4d463e762ca4d5282cb141dcb28
YuvalNirkin <ynirkin@gmail.com>
2017-01-14 10:25:40 +0200

    First prototype

--- face_swap_image.py
+++ face_swap_image.py
@@ -1,14 +1,17 @@
 import faceswap
 import argparse
 import cv2
+import matplotlib.pyplot as plt
 
 if __name__ == '__main__':
     # Parse program arguments
     parser = argparse.ArgumentParser()
     parser.add_argument('--source', '-s', required=True, help='Source image')
     parser.add_argument('--target', '-t', required=True, help='Target image')
+    parser.add_argument('--output', '-o', default='output/out.jpg', help='Output image')
     parser.add_argument('--landmarks', '-l', default='dlib_models/shape_predictor_68_face_landmarks.dat', help='Face landmarks model')
     parser.add_argument('--model3d', '-m', default='models3d/candide.npz', help='Face 3D model')
+    parser.add_argument('--bidirectional', '-b', help='Swap both directions', action="store_true")
     args = parser.parse_args()
     
     # Read source and target images
@@ -21,8 +24,50 @@ if __name__ == '__main__':
     # Do face swap
     rendered_img = fs.swap(target_img)
 
+    if args.bidirectional:
+        fs2 = faceswap.faceswap(source_img.shape[1], source_img.shape[0], target_img, args.landmarks, args.model3d)
+        rendered_img2 = fs2.swap(source_img)
+
+    # Do face swap
+    #rendered_img = fs.swap(target_img)
+    #if args.bidirectional:
+    #    rendered_img2 = fs2.swap(source_img)
+
     # Display result
-    if rendered_img is not None:
-        cv2.imshow('render', rendered_img)
-        cv2.waitKey(0)
-    else: print 'Could not find faces!'
+    if args.bidirectional:    
+        plt.subplot(231)
+        plt.title('img1')
+        plt.imshow(source_img[:, :, ::-1])
+        plt.axis('off')
+
+        plt.subplot(234)
+        plt.title('img2')
+        plt.imshow(target_img[:, :, ::-1])
+        plt.axis('off')
+
+        plt.subplot(232)
+        plt.title('img1 -> img2')
+        plt.imshow(rendered_img[:, :, ::-1])
+        plt.axis('off')
+
+        plt.subplot(235)
+        plt.title('img2 -> img1')
+        plt.imshow(rendered_img2[:, :, ::-1])
+        plt.axis('off')
+
+        plt.draw()
+        plt.pause(0.001)
+
+        # Write output image
+        plt.savefig(args.output, bbox_inches='tight')
+        
+        #enter = raw_input("Press [enter] to continue.")
+        #plt.clf()   
+    else:
+        if rendered_img is not None:
+            cv2.imshow('render', rendered_img)
+            cv2.waitKey(1)
+        else: print 'Could not find faces!'
+
+        # Write output image
+        cv2.imwrite(args.output, rendered_img)
--- face_swap_video.py
+++ face_swap_video.py
@@ -6,7 +6,8 @@ if __name__ == '__main__':
     # Parse program arguments
     parser = argparse.ArgumentParser()
     parser.add_argument('--source', '-s', required=True, help='Source image')
-    parser.add_argument('--device', '-d', default=0, type=int, help='Camera device id')
+    parser.add_argument('--target', '-t', required=True, help='Target video')
+    parser.add_argument('--output', '-o', default='output/out.mp4', help='Output video')
     parser.add_argument('--landmarks', '-l', default='dlib_models/shape_predictor_68_face_landmarks.dat', help='Face landmarks model')
     parser.add_argument('--model3d', '-m', default='models3d/candide.npz', help='Face 3D model')
     args = parser.parse_args()
@@ -14,26 +15,38 @@ if __name__ == '__main__':
     # Read source image
     source_img = cv2.imread(args.source)    
 
-    # Initialize camera
-    cap = cv2.VideoCapture(args.device)
-    frame = cap.read()[1]
+    # Initialize Video
+    cap = cv2.VideoCapture(args.target)
+    ret, frame = cap.read()
+    width = cap.get(cv2.CAP_PROP_FRAME_WIDTH)
+    height = cap.get(cv2.CAP_PROP_FRAME_HEIGHT)
+    fps = cap.get(cv2.CAP_PROP_FPS)
+    total_frames = cap.get(cv2.CAP_PROP_FRAME_COUNT)
+    vid_writer = cv2.VideoWriter(args.output, cv2.VideoWriter_fourcc('X', '2', '6', '4'), fps, (width, height))
 
     # Initialize face swap
-    fs = faceswap.faceswap(frame.shape[1], frame.shape[0], source_img, args.landmarks, args.model3d)
+    fs = faceswap.faceswap(width, height, source_img, args.landmarks, args.model3d)
 
     # Capture loop
     while True:
-        # Read camera frame
+        # Read video frame
         ret, frame = cap.read()
         if ret: break
         
         # Do face swap
         rendered_frame = fs.swap(frame)
 
+        # Write output frame
+        vid_writer.write(rendered_frame)
+
         # Display result
         cv2.imshow('frame', frame)
         if rendered_frame is not None:
             cv2.imshow('render', rendered_frame)
         if cv2.waitKey(1) >= 0: break
+
+    # Clean up
+    cap.release()
+    vid_writer.release()
 
     
